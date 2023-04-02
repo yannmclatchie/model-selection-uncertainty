8fa51a394f89b3e99be8c9242ad39a7fe717693f
Yann McLatchie <yann@mclatchie.com>
2023-04-02 14:49:39 +0300

    update triton code

--- triton/many_models_run.sh
+++ triton/many_models_run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --time=02:00:00
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=1G
+#SBATCH --mem=16G
 #SBATCH --array=1
 
 # run models
-for current_dataset in {1..100}
+for current_dataset in {1..2}
 do
-    singularity run -B /scratch,/m,/l,/share docker://andrjohns/stan-triton Rscript ./R/fit_many_models.R $1 $current_dataset $2
+    singularity run -B /scratch,/m,/l,/share docker://andrjohns/stan-triton Rscript ./R/many-irrelevant/fit_many_models.R $1 $current_dataset $2
 done
